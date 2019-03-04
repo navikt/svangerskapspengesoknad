2e20c1599cbc8681560e1d279f583a19eeafccfa
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-03-04 08:41:28 +0100

    Lag et app-banner, skap mer luft

--- src/app/connected-components/svangerskapspengesøknad/StegRoutes.tsx
+++ src/app/connected-components/svangerskapspengesøknad/StegRoutes.tsx
@@ -37,7 +37,7 @@ const StegRoutes: FunctionComponent<Props> = ({ steg, history }) => {
     };
 
     return (
-        <Applikasjonsside visSpråkvelger={true}>
+        <Applikasjonsside visSpråkvelger={true} visTittel={true}>
             <Switch>
                 <Route
                     path={søknadStegPath(StegID.FØRSTE_STEG)}
