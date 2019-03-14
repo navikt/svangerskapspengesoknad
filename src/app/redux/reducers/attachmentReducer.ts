72e1bd5978a26998b7074b5aaabe7f3c3296c570
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-03-14 09:06:19 +0100

    Refactor attachment reducer

--- src/app/redux/reducers/attachmentReducer.ts
+++ src/app/redux/reducers/attachmentReducer.ts
@@ -9,102 +9,85 @@ export interface AttachmentState {
     vedlegg: Array<Attachment>;
 }
 
+const modifyAttachmentWithId = (compareId: string, newProperties: Partial<Attachment>) => (
+    existingAttachment: Attachment
+): Attachment => {
+    return existingAttachment.id !== compareId
+        ? existingAttachment
+        : {
+              ...existingAttachment,
+              ...newProperties,
+          };
+};
+
 const attachmentReducer = (state = getDefaultAttachmentState(), action: AttachmentAction): AttachmentState => {
     switch (action.type) {
         case AttachmentActionTypes.UPLOAD_ATTACHMENT_REQUEST: {
-            return {
-                ...state,
-                vedlegg: [
-                    ...state.vedlegg,
-                    {
-                        ...action.payload.attachment,
-                        pending: true,
-                    },
-                ],
-            };
+            const { attachment } = action.payload;
+            const vedlegg = [
+                ...state.vedlegg,
+                {
+                    ...attachment,
+                    pending: true,
+                },
+            ];
+
+            return { ...state, vedlegg };
         }
 
         case AttachmentActionTypes.UPLOAD_ATTACHMENT_SUCCESS: {
             const { url, uuid, attachment } = action.payload;
+            const vedlegg = state.vedlegg.map(
+                modifyAttachmentWithId(attachment.id, {
+                    url,
+                    uuid,
+                    pending: false,
+                    uploaded: true,
+                })
+            );
 
-            return {
-                ...state,
-                vedlegg: state.vedlegg.map((v) =>
-                    v.id === attachment.id
-                        ? {
-                              ...action.payload.attachment,
-                              url,
-                              uuid,
-                              pending: false,
-                              uploaded: true,
-                          }
-                        : v
-                ),
-            };
+            return { ...state, vedlegg };
         }
 
         case AttachmentActionTypes.UPLOAD_ATTACHMENT_FAILURE: {
             const { attachment, error } = action.payload;
-            const vedlegg = state.vedlegg.map((v) =>
-                v.id === attachment.id
-                    ? {
-                          ...attachment,
-                          error: error ? error.message : '',
-                      }
-                    : v
+            const vedlegg = state.vedlegg.map(
+                modifyAttachmentWithId(attachment.id, {
+                    error: error ? error.message : '',
+                })
             );
 
-            return {
-                ...state,
-                vedlegg,
-            };
+            return { ...state, vedlegg };
         }
 
         case AttachmentActionTypes.DELETE_ATTACHMENT_REQUEST: {
             const { attachment } = action.payload;
-
-            const vedlegg = state.vedlegg.map((v) =>
-                v.id === attachment.id
-                    ? {
-                          ...action.payload.attachment,
-                          pending: true,
-                      }
-                    : v
+            const vedlegg = state.vedlegg.map(
+                modifyAttachmentWithId(attachment.id, {
+                    pending: true,
+                })
             );
 
-            return {
-                ...state,
-                vedlegg,
-            };
+            return { ...state, vedlegg };
         }
 
         case AttachmentActionTypes.DELETE_ATTACHMENT_SUCCESS: {
             const { attachment } = action.payload;
             const vedlegg = state.vedlegg.filter((v) => v.id !== attachment.id);
 
-            return {
-                ...state,
-                vedlegg,
-            };
+            return { ...state, vedlegg };
         }
 
         case AttachmentActionTypes.DELETE_ATTACHMENT_FAILURE: {
             const { attachment, error } = action.payload;
-
-            const vedlegg = state.vedlegg.map((v) =>
-                v.id === attachment.id
-                    ? {
-                          ...attachment,
-                          pending: false,
-                          error: error ? error.message : '',
-                      }
-                    : v
+            const vedlegg = state.vedlegg.map(
+                modifyAttachmentWithId(attachment.id, {
+                    pending: false,
+                    error: error ? error.message : '',
+                })
             );
 
-            return {
-                ...state,
-                vedlegg,
-            };
+            return { ...state, vedlegg };
         }
     }
 
