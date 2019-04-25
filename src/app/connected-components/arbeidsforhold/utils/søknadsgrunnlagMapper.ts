1cd62f2e7c41f158b7048ee2250dc8a6c8c8bfd7
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-04-25 12:19:39 +0200

    Change søknadsgrunnlag checkbox label for privat arbeidsgiver
    Dont show arbeidsgiver fnr. Show "privat arbeidsgiver" instead.

--- src/app/connected-components/arbeidsforhold/utils/søknadsgrunnlagMapper.ts
+++ src/app/connected-components/arbeidsforhold/utils/søknadsgrunnlagMapper.ts
@@ -19,7 +19,7 @@ export const mapArbeidsforholdToSøknadsgrunnlagOptions = (
     return [
         ...arbeidsforhold.map((forhold) => ({
             value: forhold.arbeidsgiverId,
-            label: forhold.arbeidsgiverNavn || forhold.arbeidsgiverId,
+            label: forhold.arbeidsgiverNavn || 'privat arbeidsgiver',
             type: forhold.arbeidsgiverIdType === 'orgnr' ? Arbeidsforholdstype.VIRKSOMHET : Arbeidsforholdstype.PRIVAT
         })),
         ...selvstendigNæringsdrivendeInformasjon.map((næring) => ({
