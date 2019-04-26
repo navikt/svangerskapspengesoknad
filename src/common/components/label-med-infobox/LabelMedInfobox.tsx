70d3ea531462d9ee215c2a17528aab787500943d
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-04-26 11:57:45 +0200

    Change tag type LabelMedInfobox

--- src/common/components/label-med-infobox/LabelMedInfobox.tsx
+++ src/common/components/label-med-infobox/LabelMedInfobox.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Infoboks from 'common/components/infoboks/Infoboks';
 import BEMHelper from 'common/util/bem';
+import { Element } from 'nav-frontend-typografi';
 
 import './labelMedInfobox.less';
 
@@ -8,13 +9,16 @@ interface LabelMedInfoboxProps {
     title: string | React.ReactNode;
     info?: string | React.ReactNode;
     stil?: 'normal' | 'seksjon';
+    tag?: string;
 }
 
-const LabelMedInfobox: React.StatelessComponent<LabelMedInfoboxProps> = ({ title, info, stil }) => {
+const LabelMedInfobox: React.StatelessComponent<LabelMedInfoboxProps> = ({ title, info, stil, tag = 'strong' }) => {
     const cls = BEMHelper('labelMedInfobox');
     return (
         <div className={cls.element('heading', `stil-${stil || 'normal'}`)}>
-            <h1 className={`typo-element ${cls.element('title')}`}>{title}</h1>
+            <Element className={`typo-element ${cls.element('title')}`} tag={tag}>
+                {title}
+            </Element>
             {info && <Infoboks tekst={info} contentFullWidth={true} />}
         </div>
     );
