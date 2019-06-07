49d63628afa100f0009535f7af6a161a9dbdc288
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-06-07 13:24:58 +0200

    Remove new skjemanummer
    Skjemanummer is not propery synced to kodeverk

--- src/app/connected-components/tilrettelegging/Tilrettelegging.tsx
+++ src/app/connected-components/tilrettelegging/Tilrettelegging.tsx
@@ -165,7 +165,7 @@ const Tilrettelegging: FunctionComponent<Props> = (props) => {
                                     skjemanummer={
                                         erFrilansEllerSelvstendig
                                             ? Skjemanummer.TILRETTELEGGING_FOR_FRILANS_ELLER_SELVSTENDIG
-                                            : Skjemanummer.TILRETTELEGGING_OG_OMPLASSERING_VED_GRAVIDITET
+                                            : Skjemanummer.ANNET
                                     }
                                     attachments={attachments}
                                     onFilesSelect={(files: Attachment[]) => {
