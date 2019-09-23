3fd7247b2754e8c841a28e04768ba6a9a8633bd7
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-09-23 13:57:08 +0200

    Reset initial søknad state

--- src/app/types/Søknad.ts
+++ src/app/types/Søknad.ts
@@ -1,7 +1,7 @@
 import InformasjonOmUtenlandsopphold, { InformasjonOmUtenlandsoppholdPartial } from './InformasjonOmUtenlandsopphold';
 import Barn, { UferdigBarn } from './Barn';
 import Søker, { Søkerrolle } from './Søker';
-import { UferdigTilrettelegging, Arbeidsforholdstype, Tilretteleggingstype } from './Tilrettelegging';
+import { UferdigTilrettelegging, Arbeidsforholdstype } from './Tilrettelegging';
 import { Attachment } from 'common/storage/attachment/types/Attachment';
 import { FormikErrors } from 'formik';
 import { TilretteleggingDTO } from './TilretteleggingDTO';
@@ -30,46 +30,11 @@ export interface UferdigSøknad {
 }
 
 export const initialSøknad: UferdigSøknad = {
-    harGodkjentVilkår: true,
+    harGodkjentVilkår: false,
     harGodkjentOppsummering: false,
-    barn: {
-        termindato: new Date('2019-09-30'),
-        erBarnetFødt: false
-    },
-    tilrettelegging: [
-        {
-            id: '096429518-3033-5344-0878-86180859528444',
-            vedlegg: [],
-            arbeidsforhold: {
-                id: '096429518-3033-5344-0878-86180859528444',
-                type: Arbeidsforholdstype.VIRKSOMHET
-            },
-            ingenTilrettelegging: [
-                {
-                    slutteArbeidFom: new Date('2019-09-01')
-                }
-            ],
-            delvisTilrettelegging: [
-                {
-                    stillingsprosent: 66,
-                    tilrettelagtArbeidFom: new Date('2019-09-01')
-                }
-            ],
-            helTilrettelegging: [
-                {
-                    tilrettelagtArbeidFom: new Date('2019-09-01')
-                }
-            ],
-            behovForTilretteleggingFom: new Date('2019-09-01'),
-            type: [Tilretteleggingstype.HEL, Tilretteleggingstype.DELVIS, Tilretteleggingstype.INGEN]
-        }
-    ],
-    søknadsgrunnlag: [
-        {
-            id: '096429518-3033-5344-0878-86180859528444',
-            type: Arbeidsforholdstype.VIRKSOMHET
-        }
-    ],
+    barn: {},
+    tilrettelegging: [],
+    søknadsgrunnlag: [],
     informasjonOmUtenlandsopphold: {
         jobbetINorgeSiste12Mnd: true,
         iNorgePåHendelsestidspunktet: true,
@@ -78,30 +43,10 @@ export const initialSøknad: UferdigSøknad = {
     },
     søker: {
         rolle: Søkerrolle.MOR,
-        harJobbetSomFrilansSiste10Mnd: false,
-        harJobbetSomSelvstendigNæringsdrivendeSiste10Mnd: false,
-        harHattAnnenInntektSiste10Mnd: false
+        selvstendigNæringsdrivendeInformasjon: [],
+        andreInntekterSiste10Mnd: []
     }
 };
-
-// export const initialSøknad: UferdigSøknad = {
-//     harGodkjentVilkår: false,
-//     harGodkjentOppsummering: false,
-//     barn: {},
-//     tilrettelegging: [],
-//     søknadsgrunnlag: [],
-//     informasjonOmUtenlandsopphold: {
-//         jobbetINorgeSiste12Mnd: true,
-//         iNorgePåHendelsestidspunktet: true,
-//         tidligereOpphold: [],
-//         senereOpphold: []
-//     },
-//     søker: {
-//         rolle: Søkerrolle.MOR,
-//         selvstendigNæringsdrivendeInformasjon: [],
-//         andreInntekterSiste10Mnd: []
-//     }
-// };
 
 export interface Søknadsgrunnlag {
     id: string;
