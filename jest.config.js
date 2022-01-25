3be664c23696f86d87bcc27094aecade6a01c389
Andreas Dybdahl <andreas.dybdahl@nav.no>
2022-01-25 14:30:23 +0100

    Jest changes

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
 module.exports = {
     preset: 'ts-jest/presets/js-with-ts',
-    testEnvironment: 'jsdom',
-    rootDir: './',
-    setupFilesAfterEnv: ['./jest.setup.ts'],
+    coverageReporters: ['text-summary'],
+    moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'less', 'css'],
     moduleNameMapper: {
         '\\.(css|jpg|png|svg|less)$': '<rootDir>/node_modules/jest-css-modules',
-        'nav-(.*)-style': '<rootDir>/mockFile.js',
         '^app/(.*)': '<rootDir>/src/app/$1',
         '^uttaksplan/(.*)': '<rootDir>/src/uttaksplan/$1',
-        '^common/(.*)': '<rootDir>/src/common/$1'
-    }
+        '^common/(.*)': '<rootDir>/src/common/$1',
+    },
+    rootDir: './',
+    roots: ['<rootDir>/src/app/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jsdom',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    transform: {
+        '^.+\\.(ts|tsx|js)?$': 'babel-jest',
+    },
+    transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*nav.*).*$'],
 };
